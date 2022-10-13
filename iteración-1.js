6d1cec705f98a87aa135f60afda99749e2b5b058
mariosepulveda3 <mariosepul3@gmail.com>
2022-10-13 19:39:51 +0200

    EXERCISE 2

---
+++ iteración-1.js
@@ -0,0 +1,42 @@
+// 1.1 Utiliza esta url de la api Agify 'https://api.agify.io?name=michael' para 
+// hacer un .fetch() y recibir los datos que devuelve. Imprimelo mediante un 
+// console.log(). Para ello, es necesario que crees un .html y un .js.
+
+// fetch('https://api.agify.io?name=michael')
+// .then((data) => data.json())
+// .then((data) => console.log(data))
+
+// 1.2 Dado el siguiente javascript y html. Añade la funcionalidad necesaria usando 
+// fetch() para hacer una consulta a la api cuando se haga click en el botón, 
+// pasando como parametro de la api, el valor del input.
+
+const baseUrl = 'https://api.nationalize.io/?name=';
+
+const button$$ = document.querySelector("button");
+const input$$ = document.querySelector("input");
+
+function search() {
+
+  fetch(baseUrl + input$$.value)
+    .then((data) => data.json())
+    .then((data) => console.log(data));
+};
+
+button$$.addEventListener("click", search);
+
+// 1.3 En base al ejercicio anterior. Crea dinamicamente un elemento  por cada petición 
+// a la api que diga...'El nombre X tiene un Y porciento de ser de Z' etc etc.
+// EJ: El nombre Pepe tiene un 22 porciento de ser de ET y un 6 porciento de ser 
+// de MZ.
+
+// en vez de poner consolelog en pantalla, quiero que me crees un elemento y me lo pongas debajo
+// y en ese elemento tienes que escribirme es XMLDocument.
+
+
+
+
+
+
+// 1.4 En base al ejercicio anterior, crea un botón con el texto 'X' para cada uno 
+// de los p que hayas insertado y que si el usuario hace click en este botón 
+// eliminemos el parrafo asociado.
